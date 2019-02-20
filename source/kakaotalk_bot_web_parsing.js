0a2d716d25a6bca60cd450929fc44edf675d931d
kissd3v <kissd3v@gmail.com>
2019-02-21 00:09:34 +0900

    update_web_parsing_#01

---
+++ source/kakaotalk_bot_web_parsing.js
@@ -0,0 +1,12 @@
+function response(room, msg, sender, isGroupChat, replier, imageDB) 
+{
+    if (msg=="message command reply") 
+    // if (msg.indexOf("message command reply")==0) 
+    // if (msg.indexOf("message command reply")!=-1)
+{
+    replier.reply // kakaotalk reply variable
+    (Utils.getWebText("parsing URL") // use variable get url html source tag 
+    .split("parsing html tag source part name") // use variable html source tag sort 
+    .replace(/(<([^>]+)>)/g,"")); // use variable unnecessary remove html source tag
+}
+}
