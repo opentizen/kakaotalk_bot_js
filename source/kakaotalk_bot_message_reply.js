5ea470fd9ad08b5d8f3c5f35735e5fe6b9b7ea1c
kissd3v <kissd3v@gmail.com>
2019-02-21 00:16:00 +0900

    update_source

---
+++ source/kakaotalk_bot_message_reply.js
@@ -0,0 +1,7 @@
+function response(room, msg, sender, isGroupChat, replier, imageDB) 
+{
+    if (msg=="message command reply")
+    if (msg.indexOf("message command reply")==0) 
+    if (msg.indexOf("message command reply")!=-1)
+    replier.reply("result");
+}
